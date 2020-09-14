da022ca642bc0c66f344e53effd4ab7ff7c89618
caixian <qq52577517@163.com>
2020-09-14 17:55:22 +0800

    整理接口情况

--- application/api/controller/v1/Page.php
+++ application/api/controller/v1/Page.php
@@ -10,21 +10,30 @@ class Page extends Base
     public function index()
     {
         // 首页
+        // 奖池奖品信息
+        // 抽取日志
+        // 排行Top3
     }
 
     public function friendRank()
     {
         // 裂变
+        // 个人贡献信息
+        // 好友贡献排行
     }
 
     public function rank()
     {
-        // 排行
+        // 排行 20名 积分desc
     }
 
     public function userInfo()
     {
         // 我的
+        // 个人信息
+        // 积分，钱包
+        // 签到任务完成情况
+        // 其余钥匙任务
     }
 
     public function bill()
