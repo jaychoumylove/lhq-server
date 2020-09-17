b68a623241ac89b71b5a25541a43a6cf87d44266
zlf <2878253979@qq.com>
2020-09-17 11:09:19 +0800

    增加日志记录

--- application/route.php
+++ application/route.php
@@ -27,6 +27,7 @@ Route::rule('api/:version/page/user_info', 'api/v1.Page/userInfo');
 Route::rule('api/:version/page/bill', 'api/v1.Page/bill');
 Route::rule('api/:version/page/qrcode', 'api/v1.Page/qrCode');
 Route::rule('api/:version/page/withdraw_log', 'api/v1.Page/withdrawLog');
+Route::rule('api/:version/page/log', 'api/v1.Page/log');
 Route::rule('api/:version/ad/custom', 'api/v1.Page/customAd');
 
 
