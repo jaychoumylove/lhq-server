af61f8c66522c243f6339de23cd65541ec9167c1
zlf <2878253979@qq.com>
2020-09-17 16:02:44 +0800

    完善个人信息提示

--- application/api/model/Task.php
+++ application/api/model/Task.php
@@ -32,11 +32,11 @@ class Task extends Base
             Common::res(['code' => 1, 'msg' => '请选择任务']);
         }
 
-        $avatarurl = User::where('id',$user_id)->value('avatarurl');
-        if(!$avatarurl) Common::res(['code' => 1, 'msg' => '请点击个人中心-头像完善个人信息']);
-
         switch ($type) {
             case Task::SIGN:
+                $avatarurl = User::where('id',$user_id)->value('avatarurl');
+                if(!$avatarurl) Common::res(['code' => 1, 'msg' => '请点击个人中心-头像完善个人信息']);
+
                 $res = self::settleSign($user_id);
                 break;
             case Task::INVITE:
